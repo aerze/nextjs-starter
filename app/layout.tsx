db1927be66b163f19067cc798497cce9756281c3
Abby <abby@mythril.co>
2025-03-10 22:20:32 +0000

    blind commit at chapter 4

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/ui/global.css";
+import clsx from "clsx";
+import { inter } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={clsx(inter.className, "antialiased")}>{children}</body>
+    </html>
+  );
+}
